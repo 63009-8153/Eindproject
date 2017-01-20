25ca598eaf1a082b039b2f7ac01fe64b02690705
Olivier <olie_vier@hotmail.com>
2017-01-20 09:42:30 +0100

    Wouter's changes

--- OpenGL Shaders/OpenGL Shaders/Player.h
+++ OpenGL Shaders/OpenGL Shaders/Player.h
@@ -16,40 +16,41 @@ extern float mouseSensitivity;
 
 class Player : public Model
 {
-	public:
-		Player();
-		~Player();
+public:
+	Player();
+	~Player();
 
-		void init(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, float _maxHealth);
-		void update();
+	void init(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, float _maxHealth);
+	void update();
 
-		void setAttackStrength(float dmg);
+	void setAttackStrength(float dmg);
 
-		gameobject *getAnimModel();
+	gameobject *getAnimModel();
 
-		static s_anim loadAnimations(char * animationFolder, std::vector<gameobject> &gameobjects, int startframe, int frames, double fps, bool loop);
-		static void createAnimationModels();
-		static GLuint animationTexture;
+	static s_anim loadAnimations(char * animationFolder, std::vector<gameobject> &gameobjects, int startframe, int frames, double fps, bool loop);
+	static void createAnimationModels();
+	static GLuint animationTexture;
 
-		void updateAnimation(int currentType);
-		void resetAnimation();
+	void updateAnimation(int currentType);
+	void resetAnimation();
 
-		bool canUse(glm::vec3 useObjectOrigin, float maxDist);
+	bool canUse(glm::vec3 useObjectOrigin, float maxDist);
 
-		float health, maxHealth;
-		glm::vec3 speed;
-		float attackStrength;
-		//Gun gun;
+	float health, maxHealth;
+	glm::vec3 speed;
+	float attackStrength;
 
-		int networkAnimType;
+	//Gun gun;
 
-		bool active;
+	int networkAnimType;
 
-	private:
+	bool active;
 
-		int currentAnimationType,
-			currentAnimationFrame;
-		double animationExtraTime;
+private:
+
+	int currentAnimationType,
+		currentAnimationFrame;
+	double animationExtraTime;
 };
 
 #endif
